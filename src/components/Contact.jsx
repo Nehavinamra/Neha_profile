1032a1b6397ded6eee94755842b110e2d7559200
Neha Vinamra <vinamraneha@gmail.com>
2025-04-29 16:15:11 -0400

    Deploy and push code

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,9 +37,7 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "#",
           from_email: form.email,
-          to_email: "#",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
